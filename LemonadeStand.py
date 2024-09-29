1c6f70cb0e012c2b0a3b3cd141756688fbb5bb28
Shirl Lakeway <shirllakeway@outlook.com>
2024-09-28 21:28:58 -0400

    Fixed the buy loop

--- LemonadeStand.py
+++ LemonadeStand.py
@@ -1,6 +1,4 @@
 import random
-import sys
-
 print( 
 ''' 
 ==================================================== 
@@ -19,73 +17,111 @@ username = input(str("First off, what is your name?\n"))
 standname = input(str("Next, what would you like to name your new Lemonade stand?\n"))
 
 print("Hello ",username," and welcome to your stand, ",standname,". Now that we have that out of the way, let's buy some supplies before we open for the day.", sep="")
+print("Now that we have that out of the way, let's buy some supplies before we open for the day.")
 print("You have taken a loan from the bank for $100 to start your business\n")
 
 money = 100
-supplies = [['Sugar', 'Water', 'Cups','Lemons'], [0,0,0,0]]
-
-answer = input(str("Would you like to buy supplies for today? Yes/No\n"))
-if answer in ['y', 'Y', 'yes', 'Yes', 'YES']:
-    print('For',supplies[0][0], 'press "1"')
-    print('For',supplies[0][1], 'press "2"')
-    print('For',supplies[0][2], 'press "3"')
-    print('For',supplies[0][3], 'press "4"')
-    print('"5" to stop shopping')
-    choice = int(input(''))
-    
-while choice > 5:
-    print(choice,'is not a valid option. Please try again.')
-    print('For',supplies[0][0], 'press "1"')
-    print('For',supplies[0][1], 'press "2"')
-    print('For',supplies[0][2], 'press "3"')
-    print('For',supplies[0][3], 'press "4"')
-    print('"5" to stop shopping')
-    choice = int(input(''))
-
 sugar_price = random.randint(1,5)
 water_price = random.randint(1,3)
 cups_price = random.randint(1,3)
 lemons_price = random.randint(2,7)
+supplies = [['Sugar', 'Water', 'Cups','Lemons'], [0,0,0,0]]
 
+answer = input(str("Would you like to buy supplies for today? Yes/No\n"))
+if answer in ['y', 'Y', 'yes', 'Yes', 'YES']:
+    print('For',supplies[0][0],'press 1')
+    print('For',supplies[0][1],'press 2')
+    print('For',supplies[0][2],'press 3')
+    print('For',supplies[0][3],'press 4')
+    print('To check your current supplies press 5')
+    print('To open your business for the day press 6')
+    choice = int(input(''))
     
-if choice == 1:
-    print('The price of Sugar today is: $',sugar_price, sep='')
-    print('You currently own',supplies[1][0],'bags of sugar.')
-    sugar_own = int(input('How much sugar would you like to buy today?\n'))
-    if sugar_own >= 1:
-        print('You now own',sugar_own,'bags of sugar.')
-        money = money-sugar_own*sugar_price
-        supplies[1][0] = sugar_own
-        print('You have $',money,' left.',sep = '')
-        print(supplies[1][0])
+while choice != 6:
+    if choice == 1:
+        print('The price of Sugar today is: $',sugar_price, sep='')
+        print('You currently own',supplies[1][0],'bags of sugar.')                
+        sugar_own = int(input('How much sugar would you like to buy today? Enter 0 to cancel.\n'))
+        if sugar_own != 0:
+            print('You now own',sugar_own,'bags of sugar.')
+            money = money-sugar_own*sugar_price
+            supplies[1][0] = sugar_own + supplies[1][0]
+            print('You have $',money,' left.',sep = '')
+        else:
+            print('For',supplies[0][0],'press 1')
+            print('For',supplies[0][1],'press 2')
+            print('For',supplies[0][2],'press 3')
+            print('For',supplies[0][3],'press 4')
+            print('To check your current supplies press 5')
+            print('To open your business for the day press 6')
+            choice = int(input(''))
+    elif choice == 2:
+        print("The price of Water today is: $",water_price, sep="")
+        water_own = int(input('How many water bottles would you like to buy today? Enter 0 to cancel.\n'))
+        if water_own != 0:
+            print('You now own',water_own,'bottles of water.')
+            money = money-water_own*water_price
+            supplies[1][1] = water_own + supplies[1][1]
+            print('You have $',money,' left.',sep = '')
+        else:
+            print('For',supplies[0][0],'press 1')
+            print('For',supplies[0][1],'press 2')
+            print('For',supplies[0][2],'press 3')
+            print('For',supplies[0][3],'press 4')
+            print('To check your current supplies press 5')
+            print('To open your business for the day press 6')
+            choice = int(input(''))
+    elif choice == 3:
+        print("The price of Cups today is: $",cups_price, sep="")
+        cups_own = int(input('How many cups would you like to buy today? Enter 0 to cancel.\n'))
+        if cups_own != 0:
+            print('You now own',cups_own,'cups.')
+            money = money-cups_own*cups_price
+            supplies[1][2] = cups_own + supplies[1][2]
+            print('You have $',money,' left.',sep = '')
+        else:
+            print('For',supplies[0][0],'press 1')
+            print('For',supplies[0][1],'press 2')
+            print('For',supplies[0][2],'press 3')
+            print('For',supplies[0][3],'press 4')
+            print('To check your current supplies press 5')
+            print('To open your business for the day press 6')
+            choice = int(input(''))
+    elif choice == 4:
+        print("The price of Lemons today is: $",lemons_price, sep="")
+        lemons_own = int(input('How many lemons would you like to buy today? Enter 0 to cancel.\n'))
+        if lemons_own != 0:
+            print('You now own',lemons_own,'lemons.')
+            money = money-lemons_own*lemons_price
+            supplies[1][3] = lemons_own + supplies[1][3]
+            print('You have $',money,' left.',sep = '')
+        else:
+            print('For',supplies[0][0],'press 1')
+            print('For',supplies[0][1],'press 2')
+            print('For',supplies[0][2],'press 3')
+            print('For',supplies[0][3],'press 4')
+            print('To check your current supplies press 5')
+            print('To open your business for the day press 6')
+            choice = int(input(''))
+    elif choice == 5:
+        print('You currently have:',supplies[1][0], supplies[0][0])
+        print('You currently have:',supplies[1][1], supplies[0][1])
+        print('You currently have:',supplies[1][2], supplies[0][2])
+        print('You currently have:',supplies[1][3], supplies[0][3])
+        print('')
+        print('For',supplies[0][0],'press 1')
+        print('For',supplies[0][1],'press 2')
+        print('For',supplies[0][2],'press 3')
+        print('For',supplies[0][3],'press 4')
+        print('To check your current supplies press 5')
+        print('To open your business for the day press 6')
+        choice = int(input(''))
     else:
-        print('Please enter a valid number to buy')
-elif choice == 2:
-    print("The price of Water today is: $",water_price, sep="")
-    water_own = int(input('How much sugar would you like to buy today?\n'))
-    if water_own >= 1:
-        print('You now own',water_own,'bottles of water.')
-        money = money-water_own*water_price
-        supplies[1][1] = water_own
-    else:
-        print('Please enter a valid number to buy')        
-    print('You have $',money,' left.',sep = '')
-elif choice == 3:
-    print("The price of Cups today is: $",cups_price, sep="")
-    cups_own = int(input('How much sugar would you like to buy today?\n'))
-    if cups_own >= 1:
-        print('You now own',cups_own,'cups.')
-        money = money-cups_own*cups_price
-        print('You have $',money,' left.',sep = '')
-    else:
-        print('Please enter a valid number to buy') 
-elif choice == 4:
-    print("The price of Lemons today is: $",lemon_price, sep="")
-    lemons_own = int(input('How much sugar would you like to buy today?\n'))
-    if lemons_own >= 1:
-        print('You now own',lemons_own,'lemons.')
-        money = money-lemons_own*lemons_price
-        print('You have $',money,' left.',sep = '')
-else:
-    print('Would you like to buy supplies?')
-    
+        print(choice,'is not a valid option. Please try again.')
+        print('For',supplies[0][0], 'press 1')
+        print('For',supplies[0][1], 'press 2')
+        print('For',supplies[0][2], 'press 3')
+        print('For',supplies[0][3], 'press 4')
+        print('To check your current supplies press 5')
+        print('To open your business for the day press 6')
+        choice = int(input(''))
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-This is a simple game that I'm making in Python to expand my knowledge of the language. I come from a SysAdmin role looking to move towards a devops/SWE role.
+This is a simple game that I'm making in Python to expand my knowledge of the language. I am using this to explore basic concepts while building out this game.
+
+You start with $100 and there are different supplies you can buy. Each day requires supplies to make lemonade buit the cost of the supples changes daily.
+Each day will have different temprature levels which will affect how many customers arrive at your stand.
+
+Can you make enough money to pay back your loan but also run a profitable business?
